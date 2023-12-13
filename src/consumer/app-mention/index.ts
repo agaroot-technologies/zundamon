e7a70c8210fbc601220ca8b4da838bffee1b0e22
Karibash <y.karibayashi@gmail.com>
2023-12-13 20:59:20 +0900

    feat: レスポンスに不要な記号が入ってくる場合があるのを修正する

--- src/consumer/app-mention/index.ts
+++ src/consumer/app-mention/index.ts
@@ -100,18 +100,16 @@ export const appMentionEventHandler = async (
         {tools}
 
         To use a tool, please use the following format:
-        \`\`\`
+
         Thought: Do I need to use a tool? Yes
         Action: the action to take, should be one of [{tool_names}]
         Action Input: the input to the action
         Observation: the result of the action
-        \`\`\`
 
         When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
-        \`\`\`
+
         Thought: Do I need to use a tool? No
         Final Answer: [your response here]
-        \`\`\`
 
         Begin!
 
